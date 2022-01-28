68e1e99e022d41e743eb87a2ec157a8b6e2cdd01
Jamshidbek7799 <jamshidyuldashev7799@gmail.com>
2022-01-28 15:11:02 +0500

    add 59A and 617A

---
+++ src/com/company/Problem_617A.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+public class Problem_617A {
+    // Elephant
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.println((x+4)/5);
+    }
+}

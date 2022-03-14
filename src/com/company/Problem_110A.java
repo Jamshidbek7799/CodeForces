18845fb2f0659e0c7957aa301e2259f553556e2a
Jamshidbek7799 <jamshidy353@gmail.com>
2022-03-14 19:35:30 +0500

    add new

---
+++ src/com/company/Problem_110A.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Problem_110A {
+    // Nearly Lucky Number
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str= sc.next().replaceAll("[1,2,3,5,6,8,9,0]","");
+
+        int c=str.length();
+        System.out.println(c==4||c==7?"YES":"NO");
+
+    }
+}
---
+++ src/com/company/Problem_58A.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Problem_58A {
+    // Chat room
+    public static void main(String[] args) {
+
+           Scanner sc = new Scanner(System.in);
+           String s = sc.next();
+           System.out.println(s.matches(".*h.*e.*l.*l.*o.*") ? "YES" : "NO");
+    }
+}

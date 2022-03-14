6398202c20cd27dc1a2b14a4a330290ac36d9bb9
Jamshidbek7799 <jamshidy353@gmail.com>
2022-03-14 22:17:58 +0500

    add new

--- src/com/company/Problem_266B.java
+++ src/com/company/Problem_266B.java
@@ -5,18 +5,15 @@ public class Problem_266B {
     // Queue at the School
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int t = sc.nextInt();
-//        String s=sc.next();
-//        while (t-- > 0){
-//            s=s.replaceAll("BG", "GB");
-//        }
-//        System.out.print(s+"");
 
-        String p[]= new String[n];
-        for (int i = 0; i <n; i++) {
-            p[i]=sc.next();
+        int t = sc.nextInt();
+        String s=sc.next();
+        while (t-- > 0){
+            s=s.replaceAll("BG", "GB");
         }
+        System.out.print(s+"");
+
+
 
         
 
